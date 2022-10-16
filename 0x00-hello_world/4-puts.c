906150deb278fbf9a2195409ceaae9f20bd79eb7
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-10-16 01:26:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Entry
+* main - Entry
  * Return: Always 0
- */
+**/
+
 int main(void)
+
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
