e86662dde377eee5c264c111ec0bf25408f050cb
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-10-18 13:53:45 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lower case
+ * print_alphabet - prints the alphabet in lowercase
  */
 
 void print_alphabet(void)
 {
-	char letter;
+	char la;
 
-	for (letter - 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (la	= 'a'; la <= 'z'; la++)
+		_putchar(la);
 
-		_putchar('\n');
+	_putchar('\n');
 }
