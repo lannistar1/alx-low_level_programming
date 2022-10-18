02ba3ec095d5e43b87e89c994f7f797c53d1446c
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-10-18 11:54:03 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphbet - function to print abc
- *
- * Return: Always 0
+ * print_alphabet - print all alphabet in lower case
  */
 
 void print_alphabet(void)
 {
-	char c;
-	for (c - 'a'; c < 'z'; c++)
-	{
-		_putchar(c);
-	}
+	char letter;
+
+	for (letter - 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 		_putchar('\n');
 }
