cb065da5c35a61eb67b7b0d3215a7895803a8c60
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-10-26 23:18:52 -0700

    string cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * strcat - concatinates two strings
+ * @dest: copy to destination
+ * @src: copy from source
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x06 another pointers arrays and strings
