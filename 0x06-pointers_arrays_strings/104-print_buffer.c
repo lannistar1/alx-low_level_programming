ea4c7007d9e20487eedb2d3814b8ba5fcf54d826
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-10-27 19:19:10 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,8 +18,8 @@ void print_buffer(char *b, int size)
 	{
 		for (j = 0; j < size; j += 10)
 		{
-			printf("% 8:", j);
-			for (k = j : k < j + 10; k++)
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
 			{
 				if (k % 2 == 0)
 					printf(" ");
