d562362db262a4ac70b1ae64fca2e4ab839dc34a
Siyanbola Wasiu I <waxkybrand@gmail.com>
2022-11-08 07:14:12 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,8 +2,9 @@
 
 /**
  * len - returns length of str
- * @str: string to be counted
- * Return: length of the string
+ *  @str: string to be counted
+ *
+ *  Return: length of the string
  */
 
 int len(char *str)
@@ -14,16 +15,17 @@ int len(char *str)
 	{
 		while (str[len])
 			len++;
-
 	}
 	return (len);
 }
 
 /**
- *  num_words - counts the number of words in str
+ * num_words - counts the number of words in str
  * @str: string to be used
- * Return: number of words
+ *
+ *  Return: number of words
  */
+
 int num_words(char *str)
 {
 	int i = 0, words = 0;
@@ -50,6 +52,7 @@ int num_words(char *str)
 /**
  * strtow - splits a stirng into words
  * @str: string to be splitted
+ *
  * Return: pointer to the array of splitted words
  */
 
@@ -65,7 +68,7 @@ char **strtow(char *str)
 	{
 		for (i = 0; i <= len(str) && words; i++)
 		{
-			for (i = 0; i <= len(str) && words; i++)
+			if ((str[i] != ' ') && (str[i] != '\0'))
 				size++;
 			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
 			{
@@ -90,9 +93,9 @@ char **strtow(char *str)
 				}
 			}
 		}
-		split[words] = NULL;
-		return (split);
-	}
-	else
-		return (NULLL);
+			split[words] = NULL;
+			return (split);
+		}
+		else
+			return (NULL);
 }
